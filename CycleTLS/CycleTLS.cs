2ec729e0688e9950fe0022e6b2ff373bb91c9422
Nikita Minyuk <nikita.minyuk.w@gmail.com>
2022-10-31 16:18:36 +0200

    First fill CycleTLSResponse, CycleTLSRequest, CycleTLSRequestOptions

--- CycleTLS/CycleTLS.cs
+++ CycleTLS/CycleTLS.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.Http;
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace CycleTLS
@@ -34,16 +36,31 @@ namespace CycleTLS
 
     public class CycleTLSResponse
     {
-
+        public string RequestID { get; set; }
+        public int Status { get; set; }
+        public string Body { get; set; }
+        public Dictionary<string, string> Headers { get; set; }
     }
 
     public class CycleTLSRequest
     {
-
+        public string RequestId { get; set; }
+        public CycleTLSRequestOptions Options { get; set; }
     }
 
     public class CycleTLSRequestOptions
     {
-
+        public string URL { get; set; } //`json:"url"`
+	    public string Method { get; set; } //`json:"method"`
+	    public Dictionary<string,string> Headers { get; set; } //`json:"headers"`
+	    public string Body { get; set; } //`json:"body"`
+	    public string Ja3 { get; set; } //`json:"ja3"`
+	    public string UserAgent { get; set; } //`json:"userAgent"`
+	    public string Proxy { get; set; } //`json:"proxy"`
+	    public Cookie[] Cookies { get; set; } //`json:"cookies"`
+	    public string Timeout { get; set; } //`json:"timeout"`
+	    public string DisableRedirect { get; set; } //`json:"disableRedirect"`
+	    public string[] HeaderOrder { get; set; } //`json:"headerOrder"`
+	    public string OrderAsProvided { get; set; } //`json:"orderAsProvided"` //TODO
     }
 }
